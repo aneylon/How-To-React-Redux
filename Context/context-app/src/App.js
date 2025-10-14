556baa655d4ccadbc5028a01fbd9700f365dc129
aneylon <arlen.m.neylon@gmail.com>
2025-10-14 09:21:41 -0600

    Start context examples.

---
+++ Context/context-app/src/App.js
@@ -0,0 +1,16 @@
+import BookList from "./Components/BookList";
+import Navbar from "./Components/Navbar";
+import ThemeContextProvider from "./Context/ThemeContext";
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
