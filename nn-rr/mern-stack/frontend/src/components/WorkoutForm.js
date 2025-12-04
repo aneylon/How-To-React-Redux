0711c36849da9fea7597b1b3d524a95f616b2b44
aneylon <arlen.m.neylon@gmail.com>
2025-12-03 18:53:51 -0700

    Add context.

--- nn-rr/mern-stack/frontend/src/components/WorkoutForm.js
+++ nn-rr/mern-stack/frontend/src/components/WorkoutForm.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext();
+
   const [title, setTitle] = useState("");
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
@@ -24,6 +27,7 @@ const WorkoutForm = () => {
       setLoad("");
       setReps("");
       setError(null);
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
     } else {
       console.error(json);
       setError("Error! : " + json);
--- nn-rr/mern-stack/frontend/src/pages/Home.js
+++ nn-rr/mern-stack/frontend/src/pages/Home.js
@@ -1,15 +1,22 @@
 import { useState, useEffect } from "react";
+
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null);
+  // const [workouts, setWorkouts] = useState(null);
+
+  const { workouts, dispatch } = useWorkoutsContext();
+
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch("/workouts");
       if (response.ok) {
         const json = await response.json();
-        setWorkouts(json);
+        // setWorkouts(json);
+        dispatch({ type: "SET_WORKOUTS", payload: json });
       }
     };
     fetchWorkouts();
