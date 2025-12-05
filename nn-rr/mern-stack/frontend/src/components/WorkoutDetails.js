0491f719857c06ac932e79f32eb19f7126c38f2b
aneylon <arlen.m.neylon@gmail.com>
2025-12-04 18:24:19 -0700

    Add delete reducer.

--- nn-rr/mern-stack/frontend/src/components/WorkoutDetails.js
+++ nn-rr/mern-stack/frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,18 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+  const handleClick = async () => {
+    const response = await fetch("/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      // remove from workout list
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -10,6 +24,7 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Load : </strong> {workout.load}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>delete</span>
     </div>
   );
 };
