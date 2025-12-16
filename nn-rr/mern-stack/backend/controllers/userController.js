9b6678af32a0f4f8d3b262b530067fbcc9583d61
aneylon <arlen.m.neylon@gmail.com>
2025-12-16 07:17:56 -0700

    Wire up sign up.

--- nn-rr/mern-stack/backend/controllers/userController.js
+++ nn-rr/mern-stack/backend/controllers/userController.js
@@ -12,7 +12,7 @@ const loginUser = async (req, res) => {
   try {
     const user = await User.login(email, password);
     const token = createToken(user._id);
-    res.status(200).json({ email, _id: user._id, token });
+    res.status(200).json({ email, id: user._id, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -23,7 +23,7 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
     const token = createToken(user._id);
-    res.status(200).json({ email, token });
+    res.status(200).json({ email, token, id: user._id });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- nn-rr/mern-stack/frontend/src/pages/Signup.js
+++ nn-rr/mern-stack/frontend/src/pages/Signup.js
@@ -1,12 +1,16 @@
+import { useSignup } from "../hooks/useSignup";
+
 const { useState } = require("react");
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { signup, error, isLoading } = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(email, password);
+    await signup(email, password);
   };
 
   return (
@@ -21,6 +25,7 @@ const Signup = () => {
           setEmail(e.target.value);
         }}
         value={email}
+        disabled={isLoading}
       />
       <label>Password :</label>
       <input
@@ -29,8 +34,10 @@ const Signup = () => {
         id="password"
         onChange={(e) => setPassword(e.target.value)}
         value={password}
+        disabled={isLoading}
       />
-      <button>Sign Up</button>
+      <button disabled={isLoading}>Sign Up</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
