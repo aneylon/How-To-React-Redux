25ba8146bff772a61c9da1a9ad46f835b546c835
aneylon <arlen.m.neylon@gmail.com>
2025-12-15 19:56:36 -0700

    Add Login and Signup components. Add Auth Context.

--- nn-rr/mern-stack/frontend/src/index.js
+++ nn-rr/mern-stack/frontend/src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router";
 import { WorkoutContextProvider } from "./context/WorkoutContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WorkoutContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </WorkoutContextProvider>
+    <AuthContextProvider>
+      <WorkoutContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </WorkoutContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
